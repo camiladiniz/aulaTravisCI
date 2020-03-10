4e27ea972cadddacad7e438d7b45e3be3213661b
Camila Santos Diniz <1901486@salas.aulas>
2020-03-10 20:00:09 -0300

    'erro'

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,7 +3,7 @@ def inicializar():
 	for i in range(3):
 		linha = []
 		for j in range(3):
-			linha.append(".")
+			linha.append("X")
 		tab.append(linha)
 	return tab
 
@@ -13,4 +13,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-	
